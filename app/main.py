e8ef205be72ee923c422e5cd02660e17720b0f06
Ostapenko Denys <wachax52@gmail.com>
2026-04-01 21:33:18 +0300

    implement Distance class with magic methods

--- app/main.py
+++ app/main.py
@@ -1,3 +1,50 @@
+from __future__ import annotations
+from typing import Union
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: Union) -> None:
+        self.km: Union = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Union) -> Distance:
+        value = other.km if isinstance(other, Distance) else other
+        return Distance(self.km + value)
+
+    def __iadd__(self, other: Union) -> Distance:
+        value = other.km if isinstance(other, Distance) else other
+        self.km += value
+        return self
+
+    def __mul__(self, number: Union) -> Distance:
+        return Distance(self.km * number)
+
+    def __truediv__(self, number: Union) -> Distance:
+        return Distance(round(self.km / number, 2))
+
+    def __lt__(self, other: Union) -> bool:
+        value = other.km if isinstance(other, Distance) else other
+        return self.km < value
+
+    def __gt__(self, other: Union) -> bool:
+        value = other.km if isinstance(other, Distance) else other
+        return self.km > value
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, (Distance, int, float)):
+            return NotImplemented
+        value = other.km if isinstance(other, Distance) else other
+        return self.km == value
+
+    def __le__(self, other: Union) -> bool:
+        value = other.km if isinstance(other, Distance) else other
+        return self.km <= value
+
+    def __ge__(self, other: Union) -> bool:
+        value = other.km if isinstance(other, Distance) else other
+        return self.km >= value
